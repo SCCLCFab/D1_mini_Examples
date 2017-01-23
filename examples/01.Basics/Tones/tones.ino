75e494ff21d8df494a0bf2220cfcfa49a8e97ac0
SCCLCFab <xcodelearner@SCCLC-L008.local>
2017-01-23 01:03:43 -0800

    Add buzzer example

---
+++ examples/01.Basics/Tones/tones.ino
@@ -0,0 +1,25 @@
+ /* connect piezo buzzer to ground and speakerPin.
+  *  speakerPin = 12 is the same as pin D6
+  *  See:
+  *  https://github.com/esp8266/Arduino/blob/master/variants/d1_mini/pins_arduino.h#L49-L61
+  */
+int speakerPin = 12;
+ 
+int numTones = 10;
+int tones[] = {261, 277, 294, 311, 330, 349, 370, 392, 415, 440};
+//            mid C  C#   D    D#   E    F    F#   G    G#   A
+ 
+void setup()
+{
+  for (int i = 0; i < numTones; i++)
+  {
+    tone(speakerPin, tones[i]);
+    delay(500);
+  }
+  noTone(speakerPin);
+}
+ 
+void loop()
+{
+}
+
