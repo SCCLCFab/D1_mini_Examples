8fbe310f612b3ff954306078b3499925e3340c47
SCCLCFab <xcodelearner@SCCLC-L020.local>
2017-02-01 09:52:30 -0800

    NeoPixelStuff

---
+++ LorenzoAndKevin/NeoPixelRing/NeoPixelRing.ino
@@ -0,0 +1,14 @@
+#include <Adafruit_NeoPixel.h>
+#define NUM_PIXELS 12
+#define DIN_PIN D2
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_PIXELS, DIN_PIN, NEO_GRB + NEO_KHZ800);
+int pause = 1000;
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
--- examples/01.Basics/Theramin/Theramin.ino
+++ examples/01.Basics/Theramin/Theramin.ino
@@ -4,11 +4,13 @@ Adafruit Arduino - Lesson 10. Pseudo Thermin
  
 int speakerPin = 12; /* D6 */
 int photocellPin = 0;
+int button_pin = D3;
  
 void setup()
 {
   // We'll send debugging information via the Serial monitor
-  Serial.begin(9600);   
+  Serial.begin(9600);  
+  pinMode(button_pin, INPUT); 
 }
  
 void loop()
@@ -16,8 +18,8 @@ void loop()
  int reading = analogRead(photocellPin);
  Serial.print("Analog reading = ");
  Serial.println(reading);     // the raw analog reading
-
- int pitch = 200 + reading / 4;
+ int button_reading = analogRead(button_pin); //Read button value
+ int pitch = 1224 + ((reading)-600 + (button_reading)*600) / 2;
  tone(speakerPin, pitch);
 }
 
