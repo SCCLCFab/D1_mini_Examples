89e38d9ef68bbed22d375308f2af02ec60d1efb5
SCCLCFab <smetters@Griffin.local>
2017-01-31 20:09:52 -0800

    Theramin code

---
+++ examples/01.Basics/Theramin/Theramin.ino
@@ -0,0 +1,23 @@
+/*
+Adafruit Arduino - Lesson 10. Pseudo Thermin
+*/
+ 
+int speakerPin = 12; /* D6 */
+int photocellPin = 0;
+ 
+void setup()
+{
+  // We'll send debugging information via the Serial monitor
+  Serial.begin(9600);   
+}
+ 
+void loop()
+{
+ int reading = analogRead(photocellPin);
+ Serial.print("Analog reading = ");
+ Serial.println(reading);     // the raw analog reading
+
+ int pitch = 200 + reading / 4;
+ tone(speakerPin, pitch);
+}
+
